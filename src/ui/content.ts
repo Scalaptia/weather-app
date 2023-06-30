76b3857d980e0d11049efc56beceba315d41a08d
Scalaptia <fernando@haroware.com>
2023-06-30 12:49:07 -0700

    added temperature buttons in navbar

--- src/ui/content.ts
+++ src/ui/content.ts
@@ -1,6 +1,22 @@
+import '../styles/content.css'
+
 const NavBar = (() => {
     const element = document.createElement('div');
     element.classList.add('navbar');
+
+    const temperatureButtons = document.createElement('div');
+    temperatureButtons.classList.add('temp-buttons');
+
+    const selectCelsius = document.createElement('div')
+    selectCelsius.classList.add('button')
+    selectCelsius.innerText = '°C'
+    temperatureButtons.appendChild(selectCelsius)
+    const selectFarenheit = document.createElement('div')
+    selectFarenheit.classList.add('button')
+    selectFarenheit.innerText = '°F'
+    temperatureButtons.appendChild(selectFarenheit)
+
+    element.appendChild(temperatureButtons);
 
     return {
         element,
